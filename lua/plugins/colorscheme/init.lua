92cef77c86b94910a6fa025b728693cadde736cf
Dozo <thedozocode@gmail.com>
2024-04-21 21:11:09 +0200

    add: new colorschemes

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -21,59 +21,17 @@ return {
   --   end,
   -- },
 
-  -- Dracula
-  {
-    'maxmx03/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function ()
-      ---@type dracula
-      local dracula = require "dracula"
+  -- Gruvbox Baby
+  -- {
+  --   "luisiacc/gruvbox-baby",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("gruvbox").setup()
+  --     vim.cmd([[colorscheme gruvbox-baby]])
+  --   end,
+  -- },
 
-      dracula.setup({
-      transparent = false,
-      on_colors = function (colors, color)
-        ---@type dracula.palette
-        return {
-          -- override or create new colors
-          mycolor = "#ffffff",
-        }
-      end,
-      on_highlights = function (colors, color)
-        ---@type dracula.highlights
-        return {
-          ---@type vim.api.keyset.highlight
-          Normal = { fg = colors.mycolor }
-        }
-      end,
-      plugins = {
-        ["nvim-treesitter"] = true,
-        ["nvim-lspconfig"] = true,
-        ["nvim-navic"] = true,
-        ["nvim-cmp"] = true,
-        ["indent-blankline.nvim"] = true,
-        ["neo-tree.nvim"] = true,
-        ["nvim-tree.lua"] = true,
-        ["which-key.nvim"] = true,
-        ["dashboard-nvim"] = true,
-        ["gitsigns.nvim"] = true,
-        ["neogit"] = true,
-        ["todo-comments.nvim"] = true,
-        ["lazy.nvim"] = true,
-        ["telescope.nvim"] = true,
-        ["noice.nvim"] = true,
-        ["hop.nvim"] = true,
-        ["mini.statusline"] = true,
-        ["mini.tabline"] = true,
-        ["mini.starter"] = true,
-        ["mini.cursorword"] = true,
-      }
-      })
-      vim.cmd.colorscheme 'dracula'
-      -- vim.cmd.colorscheme 'dracula-soft'
-    end
-  },
-  
   -- Catppuccin
   -- {
   --   "catppuccin/nvim",
@@ -95,4 +53,37 @@ return {
   --     vim.cmd([[colorscheme dracula]])
   --   end,
   -- },
+  
+  -- Github themes
+  -- {
+  --   'projekt0n/github-nvim-theme',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('github-theme').setup({})
+  --     vim.cmd('colorscheme github_dark')
+  --   end,
+  -- }  
+  
+  -- Miasma
+  -- {
+  --   "xero/miasma.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd("colorscheme miasma")
+  --   end,
+  -- }
+
+  -- Bamboo
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {}
+      require('bamboo').load()
+      vim.o.termguicolors = true
+    end,
+  },
 }
