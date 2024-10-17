54452f64546c6fa0a5f9f0b509fed5cc0ecd13e3
Mintei <minteidev@gmail.com>
2024-10-18 00:52:16 +0200

    add: blankline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,82 +127,96 @@ return {
   },
 
   {
-    "HiPhish/rainbow-delimiters.nvim",
+    "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
     config = function()
-      require('rainbow-delimiters.setup').setup({})
+      require("ibl").setup({
+        indent = { char = "▏" }
+      })
     end
   },
 
-  {
-    "kiyoon/treesitter-indent-object.nvim",
-    event = "VeryLazy",
-    keys = {
-      {
-        "ai",
-        function() require'treesitter_indent_object.textobj'.select_indent_outer() end,
-        mode = {"x", "o"},
-        desc = "Select context-aware indent (outer)",
-      },
-      {
-        "aI",
-        function() require'treesitter_indent_object.textobj'.select_indent_outer(true) end,
-        mode = {"x", "o"},
-        desc = "Select context-aware indent (outer, line-wise)",
-      },
-      {
-        "ii",
-        function() require'treesitter_indent_object.textobj'.select_indent_inner() end,
-        mode = {"x", "o"},
-        desc = "Select context-aware indent (inner, partial range)",
-      },
-      {
-        "iI",
-        function() require'treesitter_indent_object.textobj'.select_indent_inner(true, 'V') end,
-        mode = {"x", "o"},
-        desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
-      },
-    },
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    tag = "v2.20.8",  -- Use v2
-    event = "BufReadPost",
-    config = function()
-      vim.opt.list = true
-      vim.opt.listchars:append "space:⋅"
-      vim.opt.listchars:append "eol:↴"
+  -- {
+  --   "HiPhish/rainbow-delimiters.nvim",
+  --   event = "VeryLazy",
+  --   config = function()
+  --     require('rainbow-delimiters.setup').setup({})
+  --   end
+  -- },
 
-      require("indent_blankline").setup {
-          space_char_blankline = " ",
-          show_current_context = true,
-          show_current_context_start = true,
-      }
+  -- {
+  --   "kiyoon/treesitter-indent-object.nvim",
+  --   event = "VeryLazy",
+  --   keys = {
+  --     {
+  --       "ai",
+  --       function() require'treesitter_indent_object.textobj'.select_indent_outer() end,
+  --       mode = {"x", "o"},
+  --       desc = "Select context-aware indent (outer)",
+  --     },
+  --     {
+  --       "aI",
+  --       function() require'treesitter_indent_object.textobj'.select_indent_outer(true) end,
+  --       mode = {"x", "o"},
+  --       desc = "Select context-aware indent (outer, line-wise)",
+  --     },
+  --     {
+  --       "ii",
+  --       function() require'treesitter_indent_object.textobj'.select_indent_inner() end,
+  --       mode = {"x", "o"},
+  --       desc = "Select context-aware indent (inner, partial range)",
+  --     },
+  --     {
+  --       "iI",
+  --       function() require'treesitter_indent_object.textobj'.select_indent_inner(true, 'V') end,
+  --       mode = {"x", "o"},
+  --       desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   tag = "v2.20.8",  -- Use v2
+  --   event = "BufReadPost",
+  --   config = function()
+  --     -- vim.opt.list = true
+  --     -- vim.opt.listchars:append "space:⋅"
+  --     -- vim.opt.listchars:append "eol:↴"
 
-      -- vim.opt.termguicolors = true
-      -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-      -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+  --     -- require("indent_blankline").setup {
+  --     --     space_char_blankline = " ",
+  --     --     show_current_context = true,
+  --     --     show_current_context_start = true,
+  --     -- }
 
-      -- vim.opt.list = true
-      -- vim.opt.listchars:append "space:⋅"
-      -- vim.opt.listchars:append "eol:↴"
+  --     vim.opt.termguicolors = true
+  --     vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  --     vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+  --     vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+  --     vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+  --     vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  --     vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
-      -- -- char = "▏"
-      -- require("indent_blankline").setup {
-      --     space_char_blankline = " ",
-      --     char_highlight_list = {
-      --         "IndentBlanklineIndent1",
-      --         "IndentBlanklineIndent2",
-      --         "IndentBlanklineIndent3",
-      --         "IndentBlanklineIndent4",
-      --         "IndentBlanklineIndent5",
-      --         "IndentBlanklineIndent6",
-      --     },
-      -- }
-    end,
-  },
+  --     vim.opt.list = true
+  --     vim.opt.listchars:append "space:⋅"
+  --     vim.opt.listchars:append "eol:↴"
+
+  --     -- char = "▏"
+  --     require("indent_blankline").setup {
+  --         space_char_blankline = " ",
+  --         char_highlight_list = {
+  --             "IndentBlanklineIndent1",
+  --             "IndentBlanklineIndent2",
+  --             "IndentBlanklineIndent3",
+  --             "IndentBlanklineIndent4",
+  --             "IndentBlanklineIndent5",
+  --             "IndentBlanklineIndent6",
+  --         },
+  --     }
+  --   end,
+  -- },
 }
